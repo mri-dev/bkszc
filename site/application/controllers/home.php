08ae2aa387b1c27af077cd22a2e54d7d6bad13a6
mri-dev <pocokxtrame@gmail.com>
2019-12-04 13:01:14 +0100

    Save

--- site/application/controllers/home.php
+++ site/application/controllers/home.php
@@ -35,7 +35,7 @@ class home extends Controller{
 			if ( $news->has_news() ) {
 				while ( $news->walk() ) {
 					$hir = $news->the_news();
-					$hirek[] = (new News(false, array( 'db' => $this->db )))->get($hir[ID]);
+					$hirek[] = (new News(false, array( 'db' => $this->db )))->get($hir['ID']);
 				}
 			}
 			$this->out( 'aktual_news', $hirek );
@@ -60,7 +60,7 @@ class home extends Controller{
 			if ( $news->has_news() ) {
 				while ( $news->walk() ) {
 					$hir = $news->the_news();
-					$hirek[] = (new News(false, array( 'db' => $this->db )))->get($hir[ID]);
+					$hirek[] = (new News(false, array( 'db' => $this->db )))->get($hir['ID']);
 				}
 			}
 			$this->out( 'news', $hirek );
